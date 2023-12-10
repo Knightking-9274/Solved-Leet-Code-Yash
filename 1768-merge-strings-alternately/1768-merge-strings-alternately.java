1034cb6663a270b0072f63d5e59531d549d2c06f
Knightking-9274 <119316323+Knightking-9274@users.noreply.github.com>
2023-12-10 17:40:15 +0530

    Time: 9 ms (9.03%), Space: 41.9 MB (11.54%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        // StringBuilder res = new StringBuilder();
+        String res ="";
+        
+      for(int i=0; i<word1.length() || i<word2.length(); i++){
+          if(i<word1.length()){
+             res+=word1.charAt(i);
+          }
+           if(i<word2.length()){
+              res+=word2.charAt(i);
+          }
+      }
+        return res;
+        
+         
+    }
+}
